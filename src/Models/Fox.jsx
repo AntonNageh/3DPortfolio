c810b32ce3e74e9182befac5d10ce492c6d2dba8
AntonNageh <92064286+AntonNageh@users.noreply.github.com>
2023-12-04 18:59:12 +0200

    FixedEmailJS + removedConsole

--- src/Models/Fox.jsx
+++ src/Models/Fox.jsx
@@ -8,7 +8,7 @@ const Fox = ({currentAnimation, ...props}) => {
   const { nodes, materials, animations } = useGLTF(scene);
   const { actions } = useAnimations(animations, group);
   useEffect(()=>{
-    console.log(actions)
+
     Object.values(actions).forEach((action) => action.stop())
     if(actions[currentAnimation])
         actions[currentAnimation].play()
